d2131ae256925773a8f2bc537b49abc49ffff5fa
Kittywhiskers Van Gogh <6098974-kittywhiskers@users.noreply.gitlab.com>
2020-07-08 17:19:39 +0530

    Make compensations for memory limits and fixes

--- launchScript.sh
+++ launchScript.sh
@@ -6,7 +6,7 @@ export PATH=$PWD/ffmpegBins:$PATH
 chmod +x ./ffmpegBins/ffmpeg
 chmod +x $PWD/generateAsset.sh && $PWD/generateAsset.sh
 # Start streaming in the background and show us it is doing the job
-$PWD/ffmpegScripts/ffmpegLoopStream.sh $PWD/assets/deployedStream.mp4 24 2160 $SITE_ACCESS_KEY &
+$PWD/ffmpegScripts/ffmpegLoopStream.sh $PWD/assets/deployedStream.mp4 24 720 $SITE_ACCESS_KEY &
 jobs
 # Start PHP server
 vendor/bin/heroku-php-apache2
