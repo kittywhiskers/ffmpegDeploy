3d3a0603c5874bb3a1487e1ac8114c8facb3cd3b
Kittywhiskers Van Gogh <6098974-kittywhiskers@users.noreply.gitlab.com>
2020-07-08 14:39:53 +0530

    Actually deploy PHP instance upon launch

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bash $PWD/launchScript.sh
--- launchScript.sh
+++ launchScript.sh
@@ -2,4 +2,4 @@
 
 bash $PWD/generateAsset.sh
 nohup bash $PWD/ffmpegScripts/ffmpegLoopStream.sh $PWD/assets/deployedStream.mp4 24 2160 $SITE_ACCESS_KEY & disown
-echo "Hi"
+vendor/bin/heroku-php-apache2
